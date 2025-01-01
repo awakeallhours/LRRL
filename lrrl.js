be977729e0a0b953035a77d7fb74bb869dac7da5
Col <133714102+awakeallhours@users.noreply.github.com>
2025-01-01 01:07:17 +0000

    making the buutons functional

---
+++ lrrl.js
@@ -0,0 +1,112 @@
+// Global Variables
+
+let clicked;
+
+// Global End
+
+// Elements
+const container = document.querySelector('#container')
+
+const backing = document.createElement('div')
+backing.className = 'backing'
+container.appendChild(backing)
+
+const leftButtonContainer = document.createElement('div')
+leftButtonContainer.className = 'lCont'
+const centreDiv = document.createElement('div')
+centreDiv.className = 'centreDiv'
+const centerTxt = document.createElement('h2')
+centerTxt.textContent = 'LRRL'
+const rightButtonContainer = document.createElement('div')
+rightButtonContainer.className = 'rCont'
+
+backing.appendChild(leftButtonContainer)
+backing.appendChild(centreDiv)
+centreDiv.appendChild(centerTxt)
+backing.appendChild(rightButtonContainer)
+
+const lButton = document.createElement('button')
+lButton.className = 'button'
+lButton.id = 'lBtn'
+lButton.textContent = 'Left'
+const rButton = document.createElement('button')
+rButton.className = 'button'
+rButton.id = 'rBtn'
+rButton.textContent = 'Right'
+
+lButton.addEventListener('click', () => {
+    clicked = document.querySelector('#lBtn')
+    console.log('left click')
+    clickMove();
+    //clickShrink();
+    return clicked
+})
+
+rButton.addEventListener('click', () => {
+    clicked = document.querySelector('#rBtn')
+    clickMove();
+   // clickShrink();
+    console.log('right click')
+    return clicked
+})
+
+leftButtonContainer.appendChild(lButton)
+rightButtonContainer.appendChild(rButton)
+
+//Elements End
+
+//Functions
+
+function clickMove(button) 
+{
+    const rWidth = leftButtonContainer.clientWidth + centreDiv.clientWidth 
+    let Y = Math.floor(Math.random() * leftButtonContainer.clientHeight )
+    let LX = Math.floor(Math.random() * leftButtonContainer.clientWidth )
+    let RX = Math.floor(Math.random() * leftButtonContainer.clientWidth )
+    
+    
+    if(clicked.textContent === 'Left')
+    {
+        clicked.setAttribute("style", `top: ${Y}px; left: ${LX}px`)
+    }
+    else if(clicked.textContent === 'Right') {
+        clicked.setAttribute("style", `top: ${Y}px; left: ${RX}px`)
+    }
+} 
+
+
+function clickShrink(button) {
+    let lbuttonSize = (lButton.clientWidth, lButton.clientHeight)
+    let lButtonWidth = (lButton.clientWidth)
+    let lButtonHeight = (lButton.clientHeight)
+
+    let rButtonWidth = (lButton.clientWidth)
+    let rButtonHeight = (lButton.clientHeight)
+
+    let reduction = 10;
+
+    //neither of these options to change height worked correctly yet
+
+
+    if(clicked.textContent === 'Left')
+        {
+            clicked.setAttribute("style", `width: ${lbuttonSize}px; height: ${lbuttonSize}px; ${-reduction}`)
+            return lbuttonSize
+        }
+        else if(clicked.textContent === 'Right') {
+            clicked.setAttribute("style", `width: ${rButtonWidth -10}px`)
+            clicked.setAttribute("style", `height: ${rButtonHeight -10}px`)
+        }
+}
+function Timer() {
+
+}
+
+function Score() {
+
+}
+
+//Functions End
+
+
+
